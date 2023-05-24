e4406449180b62c164702ef565e7bb556c1014ee
kings111444 <kings111555@gmail.com>
2023-05-25 04:08:58 +0800

    Last edit

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,62 +1,89 @@
-import {useState,useEffect} from'react';
-import useAuth from './useAuth';
-import SpotifyWebApi from'spotify-web-api-node'
-import { Container, Form } from "react-bootstrap"
-import TrackSearchResult from './TrackSearchResult';
-
+import { useState, useEffect } from "react";
+import useAuth from "./useAuth";
+import SpotifyWebApi from "spotify-web-api-node";
+import Player from "./player";
+import { Container, Form } from "react-bootstrap";
+import TrackSearchResult from "./TrackSearchResult";
+import Upload from "./imgupload";
 
 const spotifyApi = new SpotifyWebApi({
-    clientId: '84bbb856f0b54f28bc26195259b2b2d9',
-})
+  clientId: "84bbb856f0b54f28bc26195259b2b2d9",
+});
 
+export default function Dashboard({ code }) {
+  const accessToken = useAuth(code);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [playingTrack, setPlayingTrack] = useState();
+  
 
-export default function Dashboard( {code} ) {
-    const accessToken = useAuth(code)
-    const [search, setSearch] = useState('')
-    const [searchResults, setSearchResults] = useState([])
-    console.log(searchResults)
-    useEffect(() => {
-        if (!accessToken) return
-        spotifyApi.setAccessToken(accessToken)
-      }, [accessToken])
+  function chooseTrack(track) {
+    setPlayingTrack(track);
+    // setSearch("");
+  }
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+
+    let cancel = false;
+    spotifyApi.searchTracks(search).then(res => {
+      if (cancel) return;
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image
+              return smallest
+            },
+            track.album.images[0]
+          )
+
+          return {
+            artist: track.artists[0].name,
+            name: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
+        })
+      );
+    });
+    return () => (cancel = true);
+  }, [search, accessToken]);
+
+  return (
     
-      useEffect(() => {
-        if (!search) return setSearchResults([])
-        if (!accessToken) return
-    
-        let cancel = false
-        spotifyApi.searchTracks(search).then(res => {
-            if (cancel) return
-            setSearchResults(res.body.tracks.items.map(track => {
-                const smallestAlbumImage = track.album.images.reduce(
-                    (smallest, image) => {
-                        if (image.height < smallest.height) return image
-                        return smallest
-                    }, track.album.images[0])
-             return{
-                artist: track.artists[0].name,
-                name: track.name,
-                uri: track.uri,
-                albumUrl: smallestAlbumImage.url
-             }   
-            })
-        )})
-        return () => (cancel = true)
-      }, [search, accessToken])
-
-      return(
-      <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
-      <Form.Control
+    <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
+      <Upload
+        onUpload={(e) => {
+          console.log(e);
+          setSearch("genre:" + e);
+        }}
+      />
+      
+       <Form.Control
         type="search"
         placeholder="Search Songs/Artists"
         value={search}
-        onChange={e => setSearch(e.target.value)}
+        onChange={(e) => setSearch(e.target.value)}
       ></Form.Control>
       <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
         {searchResults.map(track => (
-            <TrackSearchResult key={track.uri} track={track} />
+          <TrackSearchResult
+            key={track.uri}
+            track={track}
+            chooseTrack={chooseTrack}
+          />
         ))}
-        </div>
-      </Container>
-      )
+      </div>
+      <div>
+        <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+      </div>
+    </Container>
+  );
 }
